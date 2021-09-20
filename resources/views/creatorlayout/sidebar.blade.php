8e1c4903a01df13df200daddd87b90dab2815147
rajandasgupta999 <rajon168075@gmail.com>
2021-09-20 19:17:01 +0600

    Youtube-video-Converter

---
+++ resources/views/creatorlayout/sidebar.blade.php
@@ -0,0 +1,56 @@
+
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- page container area start -->
+    <div class="page-container">
+        <!-- sidebar menu area start -->
+        <div class="sidebar-menu">
+            <div class="sidebar-header">
+                <div class="logo">
+                    <a href="/dashboard"><h1><b>CMS</b></h1></a>
+                 
+                </div>
+            </div>
+            <div class="main-menu">
+                <div class="menu-inner">
+                    <nav>
+                        <ul class="metismenu" id="menu">
+                            <li class="active">
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span> Dashboard</span></a>
+                                <ul class="collapse">
+                                    {{-- <li><a href="index.blade.php"></a></li> --}}
+                                    
+                                    <li ><a href="/book">Littareture</a></li>
+                                    <li><a href="/photo">Photography</a></li>
+                                    <li><a href="/video">Cinematography</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Profile
+                                        
+                                    </span></a>
+                                <ul class="collapse">
+                                    <li><a href="/myprofile">Edit Profile</a></li>
+                               
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-pie-chart"></i><span>Order Dashboard</span></a>
+                                <ul class="collapse">
+                                    <li><a href="/orderstatus">Order Status</a></li>
+                                    
+                                </ul>
+                            </li>
+                            
+                            
+                                
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <!-- sidebar menu area end -->
